b32c36478246194f8acb18738ce4c75abadfa3a9
Amandeep Yadav <amandeepyadav79@gmail.com>
2021-06-06 01:56:47 +0530

    Update __init__.py

--- mothra/__init__.py
+++ mothra/__init__.py
@@ -14,10 +14,9 @@ inno_end=datetime(2021, 5, 16, 21, 00, 00)
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'mysecret'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
